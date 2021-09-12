d59815870a1f76f693be03d888d2995360ac8a8f
jitendrabdev <jitendradev@gmail.com>
2021-09-12 15:57:35 +0200

    Updated code for including command to run

--- tests/userLogin_test.js
+++ tests/userLogin_test.js
@@ -1,17 +1,15 @@
-Feature('userLogin');
+Feature("userLogin");
 const userInputData = require("../data/userInput");
-Before( async ({ I }) => {
-    I.amOnPage("/login");
-    let langSelList = await I.getLangList();
-    userInputData.langList = await langSelList;
-  });
+Before(async ({ I }) => {
+  I.amOnPage("/login");
+  let langSelList = await I.getLangList();
+  userInputData.langList = await langSelList;
+});
 
-Scenario('Verify user login', ({ I  }) => {
-    
-    //console.log(userInputData.langList);
-    userInputData.langList.forEach(langOption =>{
-        I.langSelect(langOption);
-        I.userLogin(userInputData.email, userInputData.password);
-        I.userLogout();
-    });
+Scenario("Verify user login", async ({ I }) => {
+  userInputData.langList.forEach((langOption) => {
+    I.langSelect(langOption);
+    I.userLogin(userInputData.email, userInputData.password);
+    I.userLogout();
+  });
 });
