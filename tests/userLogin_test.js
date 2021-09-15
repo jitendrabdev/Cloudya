af38d8ffbd30c96d2475f9f086f213fbb2fdd57f
jitendrabdev <52191984+jitendrabdev@users.noreply.github.com>
2021-09-15 12:47:49 +0200

    Update userLogin_test.js

--- tests/userLogin_test.js
+++ tests/userLogin_test.js
@@ -9,7 +9,42 @@ Before(async ({ I }) => {
 Scenario("Verify user login", ({ I }) => {
   userInputData.langList.forEach((langOption) => {
     I.langSelect(langOption);
-    I.userLogin(userInputData.email, userInputData.password);
+    I.userLogin(
+      userInputData.email,
+      userInputData.password,
+      userInputData.antiRobotBtnTex.langOption
+    );
     I.userLogout();
   });
-});
+}).tag("@login");
+
+Scenario("Verify error message for invalid login", ({ I }) => {
+  userInputData.langList.forEach((langOption) => {
+    I.langSelect(langOption);
+    I.invalidUserLogin(
+      userInputData.email,
+      "InvalidPassword",
+      userInputData.loginError.langOption
+    );
+  });
+}).tag("@invalidlogin");
+
+Scenario(
+  "Verify Anti-Robot validation button for 3 invalid login attempt",
+  ({ I }) => {
+    for (let i = 0; i < 3; i++) {
+      I.langSelect("EN");
+      I.invalidUserLogin(
+        userInputData.email,
+        "InvalidPassword",
+        userInputData.loginError.EN
+      );
+    }
+    I.antiRobot(userInputData.antiRobotBtnTex.EN);
+    I.userLogin(
+      userInputData.email,
+      userInputData.password,
+      userInputData.antiRobotBtnTex.EN
+    );
+  }
+).tag("@3invalidlogin");
