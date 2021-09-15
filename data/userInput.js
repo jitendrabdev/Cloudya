1e4c1f7bc906cf7d115c53583eef0eee110f2566
jitendrabdev <52191984+jitendrabdev@users.noreply.github.com>
2021-09-15 12:47:01 +0200

    Update userInput.js

--- data/userInput.js
+++ data/userInput.js
@@ -1,7 +1,37 @@
 const { I } = inject();
-//User data use
+
 module.exports = {
   email: "qa+EHh4@nfon.com",
   password: "soo#Kee1ae",
-  langList: ["DE", "EN", "ES", "FR","HR", "IT","NL", "PL"],
+  langList: ["DE", "EN", "ES", "FR", "HR", "IT", "NL", "PL"],
+  loginError: {
+    DE: "Benutzername und/oder Passwort ist/sind falsch",
+    EN: "Username and/or password are incorrect",
+    ES: "El nombre de usuario y/o la contraseña son incorrectos",
+    FR: "Le nom d'utilisateur et/ou le mot de passe est/sont incorrect",
+    HR: "Korisničko ime i/ili lozinka nisu ispravni",
+    IT: "Nome utente e/o password sbagliata",
+    NL: "Gebruikersnaam en/of wachtwoord zijn incorrect",
+    PL: "Nazwa użytkownika i/lub hasło są niepoprawne",
+  },
+  antiRobotBtnTex: {
+    DE: "Hier klicken",
+    EN: "Click to start verification",
+    ES: "Haga clic para iniciar la verificación",
+    FR: "Cliquez ici pour vérifier",
+    HR: "Click to start verification",
+    IT: "Clicca per iniziare",
+    NL: "Klik om te starten",
+    PL: "Click to start verification",
+  },
+  antiRobotValidationTex: {
+    DE: "Ich bin ein Mensch",
+    EN: "I am human",
+    ES: "Soy humano",
+    FR: "Je suis humain",
+    HR: "I am human",
+    IT: "Non sono un robot",
+    NL: "Ik ben een mens",
+    PL: "I am human",
+  },
 }
